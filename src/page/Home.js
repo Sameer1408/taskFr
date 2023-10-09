ba833d804b13b229656952d36faa5073bcbe8ffd
Sameer Verma <71380483+Sameer1408@users.noreply.github.com>
2023-10-10 00:30:41 +0530

    Add files via upload

--- src/page/Home.js
+++ src/page/Home.js
@@ -6,34 +6,42 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setTask } from "../state/action-creators";
 import { setUser } from "../state/action-creators";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
 
     const dispatch = useDispatch();
-    
+
     const [addTask, setAddTask] = useState(false);
     const [allTask, setAllTask] = useState([]);
+    const navigate = useNavigate();
+
 
     const getAllTask = async () => {
-        setTimeout(async()=>{
+        setTimeout(async () => {
             const jwtToken = localStorage.getItem('SavedToken');
-            const response = await axios.get('http://localhost:4000/allTasks',
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `${jwtToken}`,
-                },
-            }
+            const response = await axios.get('https://task-xjzf.onrender.com/allTasks',
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `${jwtToken}`,
+                    },
+                }
             );
             console.log(response.data);
             setAllTask(response.data);
             dispatch(setTask(response.data));
-        },[2000])
+        }, [2000])
     }
 
     useEffect(() => {
-        getAllTask();
-        dispatch(setUser());
+        const jwtToken = localStorage.getItem('SavedToken');
+        if (!jwtToken) {
+            navigate('/login')
+        }else{
+            getAllTask();
+            dispatch(setUser());
+        }
     }, [])
 
     return (
@@ -43,10 +51,10 @@ function Home() {
                 addTask ?
                     <AddTask setAddTask={setAddTask} getAllTask={getAllTask} />
                     :
-               
-                        <AllTask getAllTask={getAllTask} allTask={allTask} />
-         
-               
+
+                    <AllTask getAllTask={getAllTask} allTask={allTask} />
+
+
             }
 
         </div>
